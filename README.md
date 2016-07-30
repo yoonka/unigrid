adba84100d03b406f7dfdc3be2761ced77d87eaf
Grzegorz Junka <list1@gjunka.com>
2016-07-30 10:53:19 +0000

    Working Hello World React example using systemjs ES6 module with JSX and babel

---
+++ README.md
@@ -0,0 +1,8 @@
+Unigrid
+=======
+
+git clone https://github.com/yoonka/unigrid.git
+cd unigrid
+sudo npm install -g jspm
+npm install --save-dev jspm@beta
+jspm install
---
+++ src/component.js
@@ -0,0 +1,7 @@
+import React from 'react';
+export class HelloWorld extends React.Component {
+    render() {
+        //return React.createElement('h1', null, 'Hello World');
+        return <h1>Hello World</h1>;
+    }
+}
