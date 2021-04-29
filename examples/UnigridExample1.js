decf6e03e3f3eddf9e00be73a232c27d214625b3
artwol993 <artwol993@gmail.com>
2021-04-29 16:21:05 +0200

    added files

--- examples/UnigridExample1.js
+++ examples/UnigridExample1.js
@@ -25,7 +25,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 import React from 'react';
-import tableData from './json/tableResp1.json!';
+// import tableData from './json/tableResp1.json!';
 import {
   Unigrid,
   UnigridEmptyCell,
