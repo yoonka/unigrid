319ad38a54e6b47db78465ceec98c348436162d7
artwol993 <artwol993@gmail.com>
2021-05-06 12:57:57 +0200

    styling complete

--- react-starter/webpack.config.js
+++ react-starter/webpack.config.js
@@ -28,7 +28,20 @@ module.exports = {
                 test: /\.json$/,
                 loader: 'json-loader'
 
-            }
+            },
+            // {
+            //     test: /\.(gif|png|jpe?g|svg)$/i,
+            //     use: [
+            //         'file-loader',
+            //         {
+            //             loader: 'image-webpack-loader',
+            //             options: {
+            //                 bypassOnDebug: true, // webpack@1.x
+            //                 disable: true, // webpack@2.x and newer
+            //             },
+            //         },
+            //     ],
+            // }
         ],
     },
 
