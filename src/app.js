e08c0d3f2906f480f1916067e679a1f76076d530
woliart <artwol993@gmail.com>
2022-02-03 15:12:30 +0100

    upd

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
+
 import React from "react";
 import "../sass/App.scss";
 import { Unigrid } from "../unigrid"
 import { UnigridExamples } from '../examples/unigridExamples';
 
 function App() {
-    return (
-        <div className="examples-container">
-            <div className="examples-container-box">
-                <h1 className="examples-tittle">Unigrid Examples</h1>
-                <Unigrid />
-                <UnigridExamples />
-            </div>
-        </div >
-    )
+  return (
+    <div className="examples-container">
+      <div className="examples-container-box">
+        <h1 className="examples-tittle">Unigrid Examples</h1>
+        <Unigrid />
+        <UnigridExamples />
+      </div>
+    </div >
+  )
 }
-
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ import React from "react"
 import ReactDom from "react-dom"
 import App from "./app"
 
-
 ReactDom.render(<App />, document.getElementById('app'))
